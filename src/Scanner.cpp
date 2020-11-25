b23217dbfc664b59c7c7e9f84c592495aab11c42
Will Ruman <WRuman@users.noreply.github.com>
2020-11-24 22:42:06 -0600

    Add single-character token scanning

--- src/Scanner.cpp
+++ src/Scanner.cpp
@@ -1,12 +1,49 @@
 #include "Scanner.h"
+#include <iostream>
 
 namespace pl {
-std::vector<pl::Token> pl::Scanner::scanTokens() {
-    auto list = std::vector<pl::Token>();
+std::vector<Token> Scanner::scanTokens() {
     while (!source.eof()) {
-        source.get();
-        //list.push_back(Token(source.get()));
+        start = current;
+        scanToken();
     }
-    return list;
+    tokens.push_back(Token(TokenType::EOF, "", nullptr, line));
+    return tokens;
+}
+
+char Scanner::advance() {
+    current++;
+    char c = source.get();
+    lexeme.push_back(c);
+    return c;
+}
+
+void Scanner::addToken(TokenType t) {
+    addToken(t, nullptr);
+}
+
+void Scanner::addToken(TokenType t, std::string literal) {
+    tokens.push_back(Token(t, lexeme, literal, line));
+    lexeme = "";
+}
+
+void Scanner::scanToken() {
+    char c = advance();
+    switch (c)
+    {
+        case '(': addToken(TokenType::LEFT_PAREN); break;
+        case ')': addToken(TokenType::RIGHT_PAREN); break;
+        case '{': addToken(TokenType::LEFT_BRACE); break;
+        case '}': addToken(TokenType::RIGHT_BRACE); break;
+        case ',': addToken(TokenType::COMMA); break;
+        case '.': addToken(TokenType::DOT); break;
+        case '-': addToken(TokenType::MINUS); break;
+        case '+': addToken(TokenType::PLUS); break;
+        case ';': addToken(TokenType::SEMICOLON); break;
+        case '*': addToken(TokenType::STAR); break;
+        default:
+            std::cerr << line << "Unexpected character '" << c <<  "'" << std::endl;
+            break;
+    }
 }
 }
--- src/Scanner.h
+++ src/Scanner.h
@@ -9,7 +9,17 @@ namespace pl {
 class Scanner
 {
 private:
+    int start = 0;
+    int current = 0;
+    int line = 1;
+    std::vector<Token> tokens{};
+    std::string lexeme{""};
     std::istream& source;
+    static TokenType typeFor(char c);
+    char advance();
+    void addToken(TokenType t);
+    void addToken(TokenType t, std::string literal);
+    void scanToken();
 public:
     Scanner(std::istream& source): source(source) {};
     std::vector<Token> scanTokens();
